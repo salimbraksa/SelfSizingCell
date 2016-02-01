34b03f79fd88f29ecf598c8ab4bd97efd7bc7471
salimbraksa <salim.braksa@gmail.com>
2016-02-01 21:06:11 +0000

    Initial Commit

--- SelfSizingCell/ViewController.swift
+++ SelfSizingCell/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Table View Data Source
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("identifier")
+        return cell ?? UITableViewCell()
+    }
 
 }
 
